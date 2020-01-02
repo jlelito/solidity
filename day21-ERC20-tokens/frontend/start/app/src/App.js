0ae429b83bd36eb6c38927fbe6afccc796875336
jlelito <joshualelito@gmail.com>
2020-01-02 17:57:28 -0500

    added drizzle front-end to ERC20 token

--- day21-ERC20-tokens/frontend/start/app/src/App.js
+++ day21-ERC20-tokens/frontend/start/app/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Drizzle } from '@drizzle/store';
+import { drizzleReactHooks } from '@drizzle/react-plugin';
+import drizzleOptions from './drizzleOptions.js';
+import LoadingComponent from './LoadingComponent.js';
+import TokenMetadata from './TokenMetadata.js';
+import TokenWallet from './TokenWallet.js';
+
+const drizzle = new Drizzle(drizzleOptions);
+const { DrizzleProvider } = drizzleReactHooks;
 
 function App() {
   return (
     <div className="container">
       <h1>ERC20 Token</h1>
+      <DrizzleProvider drizzle={drizzle}>
+        <LoadingComponent>
+          <TokenMetadata />
+          <TokenWallet />
+        </LoadingComponent>
+      </DrizzleProvider>
     </div>
   );
 }
