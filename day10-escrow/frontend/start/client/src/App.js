98e5d5a2692ef716eb25f2acc105706593db3041
jlelito <joshualelito@gmail.com>
2020-01-04 16:57:50 -0500

    replacing old react hooks with updated UseState hooks

--- day10-escrow/frontend/start/client/src/App.js
+++ day10-escrow/frontend/start/client/src/App.js
@@ -1,76 +1,116 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getWeb3 } from './utils.js';
 import Escrow from './contracts/Escrow.json';
 
-class App extends Component {
-  state = {
-    web3: undefined,
-    accounts: [],
-    currentAccount: undefined,
-    contract: undefined,
-    balance: undefined
+function App() {
+  
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+  const [payer, setPayer] = useState(undefined);
+  const [payee, setPayee] = useState(undefined);
+  const [lawyer, setLawyer] = useState(undefined);
+  const [amount, setAmount] = useState(undefined);
+
+
+
+
+  useEffect(() => {
+    const init = async () => {
+
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Escrow.networks[networkId];
+      const contract = new web3.eth.Contract(
+        Escrow.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      
+      setWeb3(web3);
+      setAccounts(accounts);
+      setContract(contract);
+      
+      
     }
-
-
-  async componentDidMount() {
-    const web3 = await getWeb3();
-    const accounts = await web3.eth.getAccounts();
-
-    const networkId = await web3.eth.net.getId();
-    const deployedNetwork = Escrow.networks[networkId];
-    const contract = new web3.eth.Contract(Escrow.abi, deployedNetwork && deployedNetwork.address);
-
-    this.setState({web3, accounts, contract}, this.updateBalance);
-  }
-    
-  async updateBalance() {
-    const { contract } = this.state;
-    const balance = await contract.methods.balanceOf().call();
-    this.setState({balance});
-  }
-
-  async deposit(e){
-    e.preventDefault();
-    const { contract, accounts } = this.state;
-    await contract.methods.deposit().send({
-      from: accounts[0], 
-      value: e.target.elements[0].value
+    init();
+    window.ethereum.on('accountsChanged', accounts => {
+      setAccounts(accounts);
     });
-    this.updateBalance();
-  }
+  }, []);
 
-  async release(e){
-    e.preventDefault();
-    const { contract, accounts } = this.state;
-    await contract.methods.release().send({from: accounts[0]})
-    this.updateBalance();
-  }
-
-
-
-  render() {
-    if(!this.state.web3){
-      return <div>Loading...</div>;
+  useEffect (() => {
+    if(isReady()){
+      
+      updateContract();
     }
-    const { balance } = this.state;
+  }, [accounts, contract, web3]);
+    
+  async function updateContract() {
+    const balance = await contract.methods.balanceOf().call();
+    const payer = await contract.methods.payer().call();
+    const payee = await contract.methods.payee().call();
+    const lawyer = await contract.methods.lawyer().call();
+    const amount = await contract.methods.amount().call();
+    setBalance(balance);
+    setPayer(payer);
+    setPayee(payee);
+    setLawyer(lawyer);
+    setAmount(amount);
+  }
+
+  async function deposit(e){
+    e.preventDefault();
+    const value = e.target.elements[0].value;
+    await contract.methods.deposit().send({from: accounts[0], value: value});
+    updateContract();
+  }
+
+  async function release(e){
+    e.preventDefault();
+    await contract.methods.release().send({from: accounts[0]});
+    updateContract();
+    
+  }
+
+  const isReady = () => {
+    return (
+      typeof contract !== 'undefined' 
+      && typeof web3 !== 'undefined'
+      && typeof accounts !== 'undefined'
+      
+    );
+  }
+
+  if (!isReady()) {
+    return <div>Loading...</div>;
+  }
+
     return (
       <div className="container">
         <h1 className="text-center">Escrow</h1>
+        <h1>Current Account: {accounts[0]}</h1>
+        <p>Lawyer: {lawyer}</p>
+        <p>Payer: {payer}</p>
+        <p>Payee: {payee}</p>
+        
 
         <div className="row">
           <div className="col-sm-12">
-            <p>Balance: <b>{balance}</b> wei </p>
+            <p>Amount to be Sent to Escrow: {amount} Wei</p>
+            <p>Current Contract Balance: <b>{balance}</b> wei </p>
           </div>
         </div>
 
         <div className="row">
           <div className="col-sm-12">
-            <form onSubmit = {e => this.deposit(e)}>
+            <form onSubmit = {e => deposit(e)}>
               <div className="form-group">
                 <label htmlFor="deposit">Deposit</label>
                 <input type="number" className="form-control" id="deposit" />
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Deposit</button>
             </form>
           </div>
         </div>
@@ -79,13 +119,13 @@ class App extends Component {
 
         <div className="row">
           <div className="col-sm-12">
-             <button onClick={e => this.release(e)} type="submit" className="btn btn-primary">Release</button>
+             <button onClick={e => release(e)} type="submit" className="btn btn-primary">Release</button>
           </div>
         </div>
 
       </div>
     );
-  }
+  
 }
 
 export default App;
